45c5cb44b99a2f232e549a7f4305b97cec81e9ef
myckhel <myckhel1@hotmail.com>
2019-04-12 13:24:03 +0100

    installed npmpackages, installed mogo template, setup site

---
+++ resources/js/App.js
@@ -0,0 +1,24 @@
+require('./bootstrap');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import App from './containers/App';
+import { configureStore } from './Redux/store';
+
+const MainApp = () => (
+	<Provider store={configureStore()}>
+			<Router>
+				<Switch>
+					<Route path="/" component={App} />
+				</Switch>
+			</Router>
+	</Provider>
+);
+
+export default  ReactDOM.render(
+	<MainApp />,
+	document.getElementById("root")
+);
