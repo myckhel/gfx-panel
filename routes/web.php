089b34fba007a2da285a6d9ef919a1ef2e097e19
myckhel <myckhel1@hotmail.com>
2019-04-29 10:02:00 +0100

    added clear-cache and db migration routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//shared server clear cache
+Route::get('/clear-cache', function() {
+    return Artisan::call('cache:clear');
+    // return what you want
+});
+
+// migrate db
+Route::get('/db/migrate', function() {
+    return Artisan::call('migrate');
+});
+
+Route::get('/db/migrate/fresh', function() {
+  return Artisan::call('migrate:fresh');
+});
+
 Route::any('{query}',
   function() { return view('welcome'); })
   ->where('query', '.*');
