f7f83635ac3e2ca23cafacaaf5273d5e84332ab0
january123 <myckhel1@hotmail.com>
2019-12-11 00:55:51 +0100

    added app:reset command

---
+++ app/Console/Commands/AppResetCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Artisan;
+use Illuminate\Support\Facades\Log;
+
+class AppResetCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+     protected $description = 'Reset the app by refreshing db, force reintsall passport and ';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+      Log::debug('app:reset');
+      $output['freshDb'] = Artisan::call('migrate:fresh');
+      $output['passportDb'] = Artisan::call('migrate', [
+        '--path' => 'vendor/laravel/passport/database/migrations', '--force' => true
+      ]);
+      $output['passportInstall'] = Artisan::call('passport:install');
+      $output['eventGen'] = Artisan::call('event:generate');
+      $output['dbSeed'] = Artisan::call('db:seed');
+      // $output['MigrateDB'] = Artisan::call('migrate');
+      Log::debug($output);
+      print_r($output);
+    }
+}
